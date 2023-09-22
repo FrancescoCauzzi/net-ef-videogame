33bddb809bc5a8c70c9a57198939270f75a8d2fb
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-22 16:28:19 +0200

    feat: main program initialized

--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -1,10 +1,81 @@
-﻿namespace net_ef_videogame
+﻿using net_ef_videogame.Models;
+using static System.Console;
+namespace net_ef_videogame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // init
+            string welcomeMessage = @"Insert a command you wish to run:
+            - 1: insert a new videogame 
+            - 2: find a videogame by its id
+            - 3: find a videogame by a string snippet matching its name
+            - 4: delete a specific videogame by its id
+            - 5: insert a new software house
+            - 6: close the program";
+            WriteLine("Welcome to our videogame manager console app!");
+            Write(welcomeMessage);
+            WriteLine();
+            Write("Insert a command: ");
+            int selectedOption = InputChecker.GetIntInput();
+            WriteLine();
+            while (selectedOption != 6)
+            {
+                switch (selectedOption)
+                {
+                    case 1:
+                        WriteLine("Insert the data of a new videogame");
+                        Write("Insert the name of the videogame: ");
+                        string name = InputChecker.GetStringInput();
+                        Write("Insert the overview of the videogame: ");
+                        string overview = InputChecker.GetStringInput();
+                        Write("Insert the release dateof the videogame: ");
+                        DateTime releaseDate = InputChecker.GetDateTimeInput();
+                        Write("Insert the software house id: ");
+                        long softwareHouseId = (long)InputChecker.GetIntInput();
+
+                        break;
+                        
+
+                    case 2:
+                        Write("Insert the id of the videogame you are looking for: ");
+                        long videogameId = InputChecker.GetIntInput();
+                        
+
+
+                        break;
+
+                    case 3:
+                        Write("Insert the snippet of the name of the videogame you are looking for: ");
+                        string videogameSnippet = InputChecker.GetStringInput();
+                        
+
+                        break;
+
+                    case 4:
+                        Write("Insert the id of the videogame you want to delete: ");
+                        long idVideogameToDelete = InputChecker.GetIntInput();
+                        
+
+                        break;
+                    case 5:
+                        WriteLine("Insert the data of the new software house: ")
+
+                        break;
+
+                    default:
+                        WriteLine("Invalid option");
+                        break;
+
+                }
+                WriteLine();
+                WriteLine("What do you want to do now?");
+                WriteLine(welcomeMessage);
+                Write("Insert a command: ");
+                selectedOption = InputChecker.GetIntInput();
+                WriteLine();
+            }
+
         }
     }
 }
